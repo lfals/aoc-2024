7c6ac164ee587593fcf5bf30b13c635c3f96db82
luis amorim <luisfelipeamorim@hotmail.com>
2024-12-03 22:09:56 -0300

    Enhance Problem Dampener functionality: refactor unsafe report analysis logic to include removal of individual elements for improved safety report calculations. Add detailed checks for increasing and decreasing trends in reports.

--- day_2/part_two/main.go
+++ day_2/part_two/main.go
@@ -9,7 +9,6 @@ import (
 	"strings"
 )
 
-// TODO
 func ProblemDampener(inputFilePath string) int {
 	file, err := os.Open(inputFilePath)
 	if err != nil {
@@ -23,6 +22,7 @@ func ProblemDampener(inputFilePath string) int {
 	safeReports := 0
 	for scanner.Scan() {
 		line := scanner.Text()
+
 		parts := strings.Split(line, " ")
 
 		unsafeReports := 0
@@ -50,7 +50,50 @@ func ProblemDampener(inputFilePath string) int {
 
 		if unsafeReports == 0 && (allIncreasing == true || allDecreasing == true) {
 			safeReports += 1
+		} else {
+
+		out:
+			for k := range parts {
+
+				copy := make([]string, len(parts))
+				copy = append(copy[:0], parts...)
+				removed := append(copy[:k], copy[k+1:]...)
+
+				unsafeReportsCount := 0
+				unsafeIncreasing := 0
+				unsafeDecreasing := 0
+				for index := 0; index < len(removed)-1; index++ {
+
+					left, _ := strconv.Atoi(removed[index])
+					right, _ := strconv.Atoi(removed[index+1])
+
+					diff := float64(left) - float64(right)
+
+					positiveUnsafeDiff := math.Abs(diff)
+
+					if positiveUnsafeDiff > 3 || positiveUnsafeDiff < 1 {
+						unsafeReportsCount += 1
+					}
+
+					if math.Signbit(diff) {
+						unsafeIncreasing += 1
+					} else {
+						unsafeDecreasing += 1
+					}
+
+				}
+
+				allIncreasing := unsafeIncreasing > 0 && unsafeDecreasing == 0
+				allDecreasing := unsafeDecreasing > 0 && unsafeIncreasing == 0
+
+				if unsafeReportsCount == 0 && (allIncreasing == true || allDecreasing == true) {
+					safeReports += 1
+					break out
+				}
+
+			}
 		}
+
 	}
 	return safeReports
 }
